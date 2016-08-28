b15491e98abc9a54e58f81086f1699399539d764
Chain911 <15142399375@yeah.net>
2016-08-28 14:38:13 +0800

    first commit

---
+++ ShimmerDemo/src/com/romainpiel/shimmer/sample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.romainpiel.shimmer.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    ShimmerTextView tv;
+    Shimmer shimmer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (ShimmerTextView) findViewById(R.id.shimmer_tv);
+    }
+
+    public void toggleAnimation(View target) {
+        if (shimmer != null && shimmer.isAnimating()) {
+            shimmer.cancel();
+        } else {
+            shimmer = new Shimmer();
+            shimmer.start(tv);
+        }
+    }
+}
